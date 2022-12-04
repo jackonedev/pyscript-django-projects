40831f9d1ba992a67463c3bcd63cafb582485be0
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-04 16:41:36 -0300

    tutorial ended - test: OK

--- frontend/myapp/views.py
+++ frontend/myapp/views.py
@@ -1,10 +1,22 @@
+from random import randint
+import json
+
 from django.shortcuts import render
 from django.views.generic import View
 
+from django.http import JsonResponse
 
 class PyScriptHandlerView(View):
     def get(self, request):
+        
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+
+            number = randint(1, 10)
+            return JsonResponse({'number': number})
+
         return render(request, 'myapp/index.html')
 
-    # def post(self, request):
-    #     return render(request, 'myapp/index.html')
+
+    def post(self, request):
+        number = json.loads(request.body)['number']
+        return JsonResponse({'data': f'You sent {float(number)}'})
--- frontend/static/script.py
+++ frontend/static/script.py
@@ -1,1 +1,58 @@
-print ('Hello World!')
+from js import document
+import json
+from pyodide.http import pyfetch
+from pyodide import create_proxy
+
+
+async def make_request(url, method, body=None, headers=None):
+    default_headers = {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json'
+    }
+    
+    if headers:
+        default_headers.update(headers)
+
+    response = await pyfetch(
+        url=url,
+        method=method,
+        body=body,
+        headers=default_headers
+    )
+
+    return await response.json()
+
+
+async def get_number_on_click(e):
+    data = await make_request(url='/', method='GET')
+    ul = document.getElementById('left')
+    # ul = document.querySelector('#left')
+    li = document.createElement('li')
+    li.innerHTML = data['number']
+
+    li.addEventListener('click', create_proxy(send_number_on_click))
+
+    ul.appendChild(li)
+
+
+async def send_number_on_click(e):
+    number = e.target.innerHTML
+
+    csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+    body = json.dumps({'number': number})
+    headers = {'X-CSRFToken': csrf}
+
+    data = await make_request(url='/', method='POST', body=body, headers=headers)
+
+    ul = document.getElementById('right')
+    li = document.createElement('li')
+    li.innerHTML = data['data']
+
+    ul.appendChild(li)
+
+
+def main():
+    button = document.getElementById('button')
+    button.addEventListener("click", create_proxy(get_number_on_click))
+
+main()
