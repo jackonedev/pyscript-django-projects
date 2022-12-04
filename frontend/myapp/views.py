f5c108ac78fe13c26251729f2bf3886fa48fb839
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-04 11:06:19 -0300

    modified: myapp/views.py

--- frontend/myapp/views.py
+++ frontend/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class PyScriptHandlerView(View):
+    def get(self, request):
+        return render(request, 'myapp/index.html')
+
+    # def post(self, request):
+    #     return render(request, 'myapp/index.html')
